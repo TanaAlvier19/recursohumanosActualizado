16296ff8e561ed89a0c743b56d5369754a187365
TanaAlvier19 <tanaalvier@gmail.com>
2025-10-08 15:03:01 +0100

    profissional

--- server/settings.py
+++ server/settings.py
@@ -68,7 +68,8 @@ ROOT_URLCONF = 'server.urls'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://recursohumanos-actualizado.vercel.app',  
+    'https://recursohumanos-actualizado.vercel.app',
+    'https://avdserver.up.railway.app'  
 ]
 
 CORS_ALLOW_CREDENTIALS = True
