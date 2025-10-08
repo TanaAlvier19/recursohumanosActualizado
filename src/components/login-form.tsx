7404547f2b075e30bf48c9718613586266cac2ff
TanaAlvier19 <tanaalvier@gmail.com>
2025-10-08 15:58:12 +0100

    profissional

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -109,7 +109,7 @@ export function LoginForm({ className, ...props }: LoginFormProps) {
     }
 
     try {
-      const response = await fetch("https://avdserver.up.railway.app/login/", {
+      const response = await fetch("https://recursohumanosactualizado.onrender.com/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
